ef8cd5921ac76320170a6b6c61479eab8e45d4ac
Russell T Miller <thatguy@ruby.cat.pdx.edu>
2010-12-02 10:15:10 -0800

    python script file created

---
+++ printers.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python
+
+print("i forgot python")
+
+
+# fab check
+#lpq
+#fab160bw2
+#fab160bw1
+#fab5517clr1
+#fab8202bw1
+
+#lpr
+#>fab5517bw1- FAB 55-17 Intel Lab (west)
+#>fab5517bw2- FAB 55-17 Intel Lab (east)
+#>fab6001bw1- Circuits Lab
+#>fab6019bw1- VLSI Lab
+#>fab8201bw1- FAB Frontdesk (DOGHaus)
+
+
+# eb check
+
+# lpq
+# eb325clr1
+# eb423clr1
+
+# lpr
+#> eb325bw1
+#> eb325bw2
+#> eb423bw1
+
+

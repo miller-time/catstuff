6e08cc1ecc2d090935b783deffe3d1473b0cda7b
millertime <thatguy@ruby.cat.pdx.edu>
2010-12-02 21:14:17 -0800

    done i think

--- printers.py
+++ printers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import os
+import os, sys, time, re
+from subprocess import Popen, PIPE
 
 def fabcheck():
 
@@ -14,15 +15,13 @@ def fabcheck():
                      "fab5517clr1",
                      "fab8202bw1",
                    ]
-    # lpq check - all FAB printers
-    for printer in fab_printers:
-        print("lpq -P " + printer)
-        #os.system("lpq -P " + printer)
+    # lpq the non-lpr printers
+    for printer in fab_printers[5:]:
+        do_lpq(printer)
 
-    # lpr
+    # lpr printers that are "ready"
     for printer in fab_printers[:5]:
-        print("lpr pacman -P " + printer)
-        #os.system("lpr pacman -P " + printer)
+        do_lpr(printer)
 
 def ebcheck():
 
@@ -33,26 +32,69 @@ def ebcheck():
                     "eb423clr1",
                   ]
 
-    # lpq check
-    for printer in eb_printers:
-        print("lpq -P " + printer)
-        #os.system("lpq -P " + printer)
+    # lpq the non-lpr printers
+    for printer in eb_printers[3:]:
+        do_lpq(printer)
 
-    # lpr
+    # lpr printers that are "ready"
     for printer in eb_printers[:3]:
-        print("lpr pacman -P " + printer)
-        #os.system("lpr pacman -P " + printer)
+        do_lpr(printer)
+
+def do_lpq(printer):
+        lpq_cmd = "lpq -P " + printer
+        lpq = Popen(lpq_cmd, shell=True, stdout=PIPE)
+        lpq_out = lpq.stdout.read()
+        match = re.search(r'ready',lpq_out)
+        # tell me what's up if they're not ready
+        if not match:
+            print(lpq_out)
+        #print("lpq_cmd)     # debug print
+
+def do_lpr(printer):
+        lpq_cmd = "lpq -P " + printer
+        lpq = Popen(lpq_cmd, shell=True, stdout=PIPE)
+        lpq_out = lpq.stdout.read()
+        match = re.search(r'ready',lpq_out)
+        if match:
+            generate_image(printer)
+            os.system("lpr pacman.tmp -P " + printer)
+            #print("lpr pacman.tmp -P " + printer)   #debug print
+            delete_image()
+        else:
+            print(lpq_out)
 
 def fab_or_eb():
-    
-    return result
+    result = raw_input("EB or FAB? ")
+    return result.lower()
+
+def getarg():
+    if len(sys.argv) > 1:
+        return sys.argv[1].lower()
+
+def generate_image(printer):
+    f = open('pacman.tmp', 'w')
+    f.write("Russell Miller thatguy@cat.pdx.edu " +
+            time.ctime() + " " + printer + "\n")
+    f.close()
+    os.system("cat pacman >> pacman.tmp")
+
+def delete_image():
+    if os.path.exists("pacman.tmp"):
+        os.system("rm pacman.tmp")
 
 def main():
-    
+   
     if len(sys.argv) == 1:
-        fab_or_eb()
-    fabcheck()
-    ebcheck()
+        which = fab_or_eb()
+    else:
+        which = getarg()
+    if which == "fab":
+        fabcheck()
+    elif which == "eb":
+        ebcheck()
+    else:
+        sys.exit(0)
+    print("done.")
 
 if __name__=="__main__":
     main()
